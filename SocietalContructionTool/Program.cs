476c997223fe73ab62103276e467114bfaa4a7ad
Bliztle <asbjoern.r.e@gmail.com>
2024-03-11 15:02:07 +0100

    Print AST from c# project

--- SocietalContructionTool/Program.cs
+++ SocietalContructionTool/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+void SctParseMethod()
+{
+    string input = File.ReadAllText("parser/society.sct");
+    ICharStream stream = CharStreams.fromString(input);
+    ITokenSource lexer = new SctLexer(stream);
+    ITokenStream tokens = new CommonTokenStream(lexer);
+    SctParser parser = new SctParser(tokens);
+    IParseTree tree = parser.start();
+    Console.WriteLine(tree.ToStringTree(parser));
+}
+
+SctParseMethod();
