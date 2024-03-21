6d00d8efe19c4df787129013a6837cf1b4ab970e
Markus Dam <markus.dam123@gmail.com>
2024-03-21 11:28:58 +0100

    Add type table

---
+++ SocietalConstructionTool/Compiler/Exceptions/InvalidTypeException.cs
@@ -0,0 +1,13 @@
+namespace Sct.Compiler.Exceptions
+{
+    public class InvalidTypeException : Exception
+    {
+        public InvalidTypeException()
+        {
+        }
+
+        public InvalidTypeException(string message) : base(message)
+        {
+        }
+    }
+}
