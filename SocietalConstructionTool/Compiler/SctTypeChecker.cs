ca974366d1d993b87a200c712a1c1ef8805a5670
HPT-I <isak@hauptmann.tech>
2024-03-25 13:18:01 +0100

    started typechecker implementation

--- SocietalConstructionTool/Compiler/SctTypeChecker.cs
+++ SocietalConstructionTool/Compiler/SctTypeChecker.cs
@@ -1,5 +1,31 @@
+
+using Antlr4.Runtime.Misc;
+
+using Sct.Compiler.Exceptions;
+
 namespace Sct.Compiler
 {
-    public class SctTypeChecker : SctBaseListener
-    { }
+    public class SctTypeChecker : SctBaseVisitor<SctType>
+    {
+        private readonly TypeTable _typeTable = new();
+
+        public List<InvalidTypeException> Errors = new List<InvalidTypeException>();
+
+        public override SctType VisitVariableDeclaration([NotNull] SctParser.VariableDeclarationContext context)
+        {
+
+            string typeName = context.type().GetText();
+            SctType type = _typeTable.GetType(typeName) ?? throw new InvalidTypeException($"Type {typeName} does not exist");
+            if (type == _typeTable.GetType("Predicate"))
+            {
+                Errors.Add(new InvalidTypeException("Variable cannot have a Predicate type"));
+            }
+            SctType expressionType = Visit(context.expression()); // This should call our overridden VisitExpression method with the expression context.
+            if (type != expressionType)
+            {
+                Errors.Add(new InvalidTypeException($"Type mismatch: {type} != {expressionType}"));
+            }
+            return type;
+        }
+    }
 }
