eb63527f2986df0f01e5fb0a2469a7dec8eb5226
Bliztle <asbjoern.r.e@gmail.com>
2024-04-01 09:57:26 +0200

    Added Enter and destroy

--- SocietalConstructionTool/Runtime/BaseAgent.cs
+++ SocietalConstructionTool/Runtime/BaseAgent.cs
@@ -5,4 +5,11 @@ public abstract class BaseAgent(string state, IDictionary<string, dynamic> field
     public IDictionary<string, dynamic> Fields { get; set; } = fields.ToDictionary(x => x.Key, x => x.Value);
     public abstract void Update(IRuntimeContext ctx);
     public abstract BaseAgent Clone();
+    // Public to allow nameof during compilation.
+    // Should only ever be called in a protected manner.
+    public static string EnterMethodName => nameof(Enter);
+    protected void Enter(IRuntimeContext ctx)
+    {
+        ctx.AgentHandler.CreateAgent(this);
+    }
 }
