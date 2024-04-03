0fb0fe9960e95a4eb890dd4d25cde85c786ac04d
thom268f <tthors22@student.aau.dk>
2024-04-03 12:58:26 +0200

    TypecheckerTests.cs works now

--- SocietalConstructionToolTests/TypecheckerTests.cs
+++ SocietalConstructionToolTests/TypecheckerTests.cs
@@ -23,33 +23,33 @@ namespace SocietalConstructionToolTests
         {
             UseProjectRelativeDirectory("Snapshots/TypeCheckerTests"); // save snapshots here
 
-            // string input = await File.ReadAllTextAsync(testFile);
-            // ICharStream stream = CharStreams.fromString(input);
-            // ITokenSource lexer = new SctLexer(stream);
-            // ITokenStream tokens = new CommonTokenStream(lexer);
+            string input = await File.ReadAllTextAsync(testFile);
+            ICharStream stream = CharStreams.fromString(input);
+            ITokenSource lexer = new SctLexer(stream);
+            ITokenStream tokens = new CommonTokenStream(lexer);
 
-            // List<CompilerError> errors = new();
-            // SctParser parser = new(tokens);
-            // // SctParser.StartContext result = parser.start();
-            // var returnChecker = new SctReturnCheckVisitor();
-            // errors.AddRange(returnChecker.Errors);
+            List<CompilerError> errors = new();
+            SctParser parser = new(tokens);
+            // SctParser.StartContext result = parser.start();
+            var returnChecker = new SctReturnCheckVisitor();
+            errors.AddRange(returnChecker.Errors);
 
-            // parser.Reset();
-            // var sctTableVisitor = new SctTableVisitor();
-            // _ = sctTableVisitor.Visit(parser.start());
-            // var ctable = sctTableVisitor.Ctable;
-            // errors.AddRange(sctTableVisitor.Errors);
+            parser.Reset();
+            var sctTableVisitor = new SctTableVisitor();
+            _ = sctTableVisitor.Visit(parser.start());
+            var ctable = sctTableVisitor.Ctable;
+            errors.AddRange(sctTableVisitor.Errors);
 
-            // parser.Reset();
-            // var sctTypeChecker = new SctTypeChecker(ctable);
-            // _ = parser.start().Accept(sctTypeChecker);
-            // parser.Reset();
+            parser.Reset();
+            var sctTypeChecker = new SctTypeChecker(ctable);
+            _ = parser.start().Accept(sctTypeChecker);
+            parser.Reset();
 
-            // errors.AddRange(sctTypeChecker.Errors);
+            errors.AddRange(sctTypeChecker.Errors);
 
-            // var errorString = string.Join(Environment.NewLine, errors.Select(e => e.ToString()));
+            var errorString = string.Join(Environment.NewLine, errors.Select(e => e.ToString()));
 
-            _ = await Verify("errorString")
+            _ = await Verify(errorString)
                 .UseFileName(Path.GetFileNameWithoutExtension(testFile));
         }
     }
