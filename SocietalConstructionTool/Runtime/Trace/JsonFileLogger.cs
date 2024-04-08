efbb73e6c0e392aaa770f34d71538121c2d41523
Markus Dam <markus.dam123@gmail.com>
2024-04-08 13:58:45 +0200

    Fix logging (#164)

--- SocietalConstructionTool/Runtime/Trace/JsonFileLogger.cs
+++ SocietalConstructionTool/Runtime/Trace/JsonFileLogger.cs
@@ -1,17 +1,18 @@
+using System.Text;
 using System.Text.Json;
 
 namespace Sct.Runtime.Trace
 {
     public class JsonFileLogger(string destination) : IOutputLogger
     {
-        private readonly List<IEnumerable<BaseAgent>> _ticks = [];
+        private readonly StringBuilder _builder = new();
 
         public void OnExit()
         {
             using FileStream file = File.OpenWrite(destination);
-            JsonSerializer.Serialize(file, _ticks);
+            file.Write(Encoding.UTF8.GetBytes(_builder.ToString()));
         }
 
-        public void OnTick(IRuntimeContext context) => _ticks.Add(context.AgentHandler.Agents);
+        public void OnTick(IRuntimeContext context) => _builder.Append(JsonSerializer.Serialize(context.AgentHandler.Agents));
     }
 }
