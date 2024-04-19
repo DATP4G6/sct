a278de3d4ac539acaf56e5e885db83d77ae068eb
Bliztle <84600908+Bliztle@users.noreply.github.com>
2024-04-19 11:55:55 +0200

    exit runtime if no agents (#190) (#195)

--- SocietalConstructionToolTests/RuntimeTests.cs
+++ SocietalConstructionToolTests/RuntimeTests.cs
@@ -19,18 +19,20 @@ namespace SocietalConstructionToolTests
 
             return contextMock;
         }
+        private static Mock<BaseAgent> GetAgentMock() => new("", new Dictionary<string, dynamic>());
 
         [TestMethod]
         public void TestRuntimeExits()
         {
             // Arrange
             var contextMock = GetContextMock();
+            var agentMock = GetAgentMock();
 
             _ = contextMock.SetupSequence(c => c.ShouldExit)
                 .Returns(false)
                 .Returns(true);
 
-            _ = contextMock.Setup(c => c.AgentHandler.Agents).Returns([]);
+            _ = contextMock.Setup(c => c.AgentHandler.Agents).Returns([agentMock.Object]);
 
             var runtime = new Runtime();
 
@@ -40,7 +42,43 @@ namespace SocietalConstructionToolTests
             // Assert
             contextMock.Verify(c => c.ShouldExit, Times.Exactly(2));
             contextMock.Verify(c => c.GetNextContext(), Times.Once);
-            contextMock.Verify(c => c.AgentHandler.Agents, Times.Once);
+            contextMock.Verify(c => c.AgentHandler.Agents, Times.Exactly(2));
+            contextMock.Verify(c => c.OnTick(), Times.Exactly(2));
+            contextMock.Verify(c => c.OnExit(), Times.Once);
+            contextMock.VerifyNoOtherCalls();
+        }
+
+        [TestMethod]
+        public void TestRuntimeExitsWhenNoAgents()
+        {
+            // Arrange
+            var contextMock = GetContextMock();
+            var agentMock = GetAgentMock();
+
+            // Need to return an agent twice.
+            // First time when checking while expression, second time when updating agents inside loop
+            // This stops after a single iteration
+            _ = contextMock.SetupSequence(c => c.AgentHandler.Agents)
+                .Returns([agentMock.Object])
+                .Returns([agentMock.Object])
+                .Returns([]);
+
+            // This only exists to ensure the loop doesn't run forever if the simulation doesn't exit properly
+            // If it fails, this ensures the test stops after the 2nd iteration
+            _ = contextMock.SetupSequence(c => c.ShouldExit)
+                .Returns(false)
+                .Returns(false)
+                .Returns(true);
+
+            var runtime = new Runtime();
+
+            // Act
+            runtime.Run(contextMock.Object);
+
+            // Assert
+            contextMock.Verify(c => c.ShouldExit, Times.Exactly(2));
+            contextMock.Verify(c => c.GetNextContext(), Times.Once);
+            contextMock.Verify(c => c.AgentHandler.Agents, Times.Exactly(3));
             contextMock.Verify(c => c.OnTick(), Times.Exactly(2));
             contextMock.Verify(c => c.OnExit(), Times.Once);
             contextMock.VerifyNoOtherCalls();
@@ -55,7 +93,7 @@ namespace SocietalConstructionToolTests
                 .Returns(false)
                 .Returns(true);
 
-            var agentMock = new Mock<BaseAgent>("", new Dictionary<string, dynamic>());
+            var agentMock = GetAgentMock();
             _ = contextMock.Setup(c => c.AgentHandler.Agents).Returns([agentMock.Object]);
 
             var runtime = new Runtime();
@@ -97,6 +135,10 @@ namespace SocietalConstructionToolTests
 
             var loggerMock = new Mock<IOutputLogger>();
             _ = contextMock.Setup(c => c.OutputLogger).Returns(loggerMock.Object);
+
+            // Add agent mock so it doesn't stop the runtime early
+            var agentMock = GetAgentMock();
+            _ = contextMock.Setup(c => c.AgentHandler.Agents).Returns([agentMock.Object]);
 
             var runtime = new Runtime();
 
