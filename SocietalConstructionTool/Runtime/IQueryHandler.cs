0da99fa12bb5b373b864b1235cd8c7d6ae5af2cf
Markus Dam <markus.dam123@gmail.com>
2024-04-24 15:05:44 +0200

    Fix type of `exists` and `count`

--- SocietalConstructionTool/Runtime/IQueryHandler.cs
+++ SocietalConstructionTool/Runtime/IQueryHandler.cs
@@ -2,7 +2,7 @@ namespace Sct.Runtime
 {
     public interface IQueryHandler
     {
-        public int Count(IRuntimeContext ctx, IQueryPredicate predicate);
-        public bool Exists(IRuntimeContext ctx, IQueryPredicate predicate);
+        public long Count(IRuntimeContext ctx, IQueryPredicate predicate);
+        public long Exists(IRuntimeContext ctx, IQueryPredicate predicate);
     }
 }
